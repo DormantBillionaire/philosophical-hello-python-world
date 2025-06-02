6c67567da00d57d9d38887384d4fe0fbda900149
Aspen Johnson <aspenjohnson2024@gmail.com>
2025-06-02 14:13:06 -0400

    Wrote the whole code, but a little skeptical on whether I should type cast my users scale rating from a strin to an int

---
+++ mainprogram.py
@@ -0,0 +1,16 @@
+def hello_philosophy_world():
+      initial_greeting = input("Hello, Pyth- uhm who are you? ")
+
+      initial_response = print("Well, hello" ,initial_greeting,"I didn't expect to see you here")
+      initial_question = input("Anywho, how's your life been on a scale of 1 to 10?")
+
+      if initial_question < 3: 
+            print("That which does not kill us makes us stronger")
+      if initial_question < 5:
+            print(" The mind is everything, What you think you become")
+      if initial_question < 7: 
+            print("When you have a dream, you've got to grab it and never let it go")
+      if initial_question < 11: 
+            print(" The most important thing is to enjoy your life-to be happy- it's all that matters")
+
+      
