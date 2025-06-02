1f96f7141a76e4b4fc8a9dfa4b2ed330c42fa64d
Aspen Johnson <aspenjohnson2024@gmail.com>
2025-06-02 14:42:31 -0400

    solid work, had the program finished after 10 minutes, but spent 10 more minutes learning a hard lesson on the importance of calling the function at the end of the code, lol

--- mainprogram.py
+++ mainprogram.py
@@ -1,16 +1,26 @@
-def hello_philosophy_world():
+def main():
       initial_greeting = input("Hello, Pyth- uhm who are you? ")
 
-      initial_response = print("Well, hello" ,initial_greeting,"I didn't expect to see you here")
-      initial_question = input("Anywho, how's your life been on a scale of 1 to 10?")
+      initial_response = print("Well, hello" , initial_greeting,"I didn't expect to see you here! Welcome to the Gen-Phil-ator!")
+      initial_question = input("Anywho, tell me how your day has been on a scale of 1 to 10? I'll match you to the quote to keep you high, or get you out the hell hole you're in!: ")
+      rating_response = int(initial_question)
 
-      if initial_question < 3: 
+      if rating_response < 3: 
             print("That which does not kill us makes us stronger")
-      if initial_question < 5:
+      if rating_response < 5:
             print(" The mind is everything, What you think you become")
-      if initial_question < 7: 
+      if rating_response < 7: 
             print("When you have a dream, you've got to grab it and never let it go")
-      if initial_question < 11: 
+      if rating_response < 11: 
             print(" The most important thing is to enjoy your life-to be happy- it's all that matters")
+      if rating_response < 0: 
+            print(" A negative? really, it cant have been that bad")
+      elif rating_response == str: 
+            print("I dont even want to know what that said, but it is illegal to my programing ears.. luckily")
+
+if __name__ == "__main__":
+      main()
+
+
 
       
